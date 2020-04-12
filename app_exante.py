3d2ffbcf4218d22cde91b1550b7257daee7b760d
Chen <cshen28@gmail.com>
2020-04-12 17:38:18 -0400

    first commit, pre-deploy stage

---
+++ app_exante.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 07:57:02 2020
+
+@author: 330411836
+"""
+    
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+import dash_html_components as html
+import dash_core_components as dcc
+import dash
+import ALM_kit
+import numpy as np
+from scipy.optimize import minimize
+import Backend_Deterministic as BE
+#Dash
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Link", href="#")),
+        dbc.DropdownMenu(
+            nav=True,
+            in_navbar=True,
+            label="Menu",
+            children=[
+                dbc.DropdownMenuItem("Entry 1"),
+                dbc.DropdownMenuItem("Entry 2"),
+                dbc.DropdownMenuItem(divider=True),
+                dbc.DropdownMenuItem("Entry 3"),
+            ],
+        ),
+    ],
+    brand="Demo",
+    brand_href="#",
+    sticky="top",
+)
+
+body = dbc.Container(
+    [dbc.Row(
+        [
+            dbc.Col(
+                [
+                    dcc.Graph(id='graph_YcReturnAlloc', figure=BE.fig)
+                ]
+            )
+        ]
+    ),
+        dbc.Row([dbc.Col([
+            html.Div(id='time_range_select', children='Time Range: ?, ?'),
+            dcc.RangeSlider(
+                 id='time_range',
+                 min=0,
+                 max=BE.fit_par.shape[0]-1,
+                 step=1,
+                 value=[1, 100]
+                 )
+        ]),
+            dbc.Col([
+                html.Div([
+                    html.Div(
+                        id='Asset_mix', children='Bond Mix: [Long, Median, Short]'),
+                    html.Div(dcc.Input(id='asset_mix_input',
+                                       type='text',value='1 1 1'))
+                ], style={'columnCount': 2}
+                ),
+                html.Button('Duration match', id='Op'),
+                html.Button('Submit', id='button'),
+                html.Div(id='asset_dur', children=''),
+                html.Div(id='liability_dur', children='')
+            ]
+        )
+        ])
+    ]
+)
+
+app.layout = html.Div([navbar, body])
+
+
+@app.callback(
+    [Output(component_id='graph_YcReturnAlloc', component_property='figure'),
+     Output(component_id='asset_dur', component_property='children'),
+     Output(component_id='liability_dur', component_property='children'),
+     Output(component_id='asset_mix_input', component_property='value'),
+     Output(component_id='time_range_select', component_property='children')],
+    [Input(component_id='time_range', component_property='value'),
+     Input(component_id='button', component_property='n_clicks'),
+     Input(component_id='Op', component_property='n_clicks')],
+    [State(component_id='asset_mix_input', component_property='value')]
+)
+
+def update_figure(time_range, n_clicks_submit, n_clicks_duration, Asset_text):
+
+    #Inputs
+    #Input for asset mixes
+    asset_mix = np.fromstring(Asset_text, dtype=float, sep=' ')
+
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        button_id = 'No clicks yet'
+    else:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    #Input for yield t1 and t2
+    t1 = time_range[0]
+    t2 = time_range[1]
+
+    time_range_t1t2 = 'Range Slider: ' + ' y1 <' + str(BE.t_cal.iloc[t1]) + '> y2 <' + str(BE.t_cal.iloc[t2]) + '>'
+
+#Recalculate
+    #Extract new points for Yield Curve plot
+    y1 = BE.y[t1]
+    y2 = BE.y[t2]
+    fit_par_t1 = BE.fit_par[t1]
+    fit_par_t2 = BE.fit_par[t2]
+
+    #Recalculate values for tables
+    #Duration recalculate
+    PV_Lt1, dur_liabilities_t1 = ALM_kit.PV_cashflow(
+        BE.cf_liability, BE.t_liability, fit_ns=BE.fit_par[t1])
+
+    if button_id == 'Op':
+        def res_func(x): return ALM_kit.optimize_duration(
+            x, BE.cf_bonds, BE.t_bond_L, BE.fit_par[t1], dur_liabilities_t1)
+        res = minimize(res_func, asset_mix, method='nelder-mead',
+                       options={'xatol': 1e-8, 'disp': True})
+        asset_mix = abs(res.x)
+        cf_weights = abs(res.x)  # Bond weight (Duration matched)
+    else:
+        cf_weights = asset_mix
+
+    cf_bond_single = np.dot(cf_weights, BE.cf_bonds)
+    PV_At1, _ = ALM_kit.PV_cashflow(
+        cf_bond_single, BE.t_bond_L, fit_ns=BE.fit_par[t1])
+    nBond = PV_Lt1/PV_At1
+
+    cf_bond = cf_bond_single*nBond
+    _, dur_asset_t1 = ALM_kit.PV_cashflow(
+        cf_bond, BE.t_bond_L, fit_ns=BE.fit_par[t1])
+    _, dur_liabilities_t2 = ALM_kit.PV_cashflow(
+        BE.cf_liability, BE.t_liability, fit_ns=BE.fit_par[t2])
+    _, dur_asset_t2 = ALM_kit.PV_cashflow(
+        cf_bond, BE.t_bond_L, fit_ns=BE.fit_par[t2])
+
+    #Asset mixes selection
+    duration_asset_text = "Dur_asset @ y1: {:.2f}, y2: {:.2f}".format(
+        dur_asset_t1, dur_asset_t2)
+    duration_liability_text = "Dur_liability @ y1: {:.2f}, y2: {:.2f}".format(
+        dur_liabilities_t1, dur_liabilities_t2)
+
+    #Asset allocation recalculate
+    test_asset, test_liabiltiy, _, _ = ALM_kit.FactorAnalysis(BE.fit_par, t1, t2,
+                                                              cf_asset=(
+                                                                  cf_bond), t_asset=BE.t_bond_L,
+                                                              cf_liability=BE.cf_liability, t_liability=BE.t_liability)
+    #Table values recalculate
+
+    test_asset_perc = np.append(
+        np.exp(np.diff(np.log(test_asset)))-1, test_asset[-1]/test_asset[0]-1)*100
+    test_liabiltiy_perc = np.append(np.exp(
+        np.diff(np.log(test_liabiltiy)))-1, test_liabiltiy[-1]/test_liabiltiy[0]-1)*100
+
+    PV_asset_t1t2 = [test_asset[0], test_asset[-1]]
+    PV_liabilities_t1t2 = [test_liabiltiy[0], test_liabiltiy[-1]]
+    PV_ALMis = [test_asset[0]-test_liabiltiy[0],
+                test_asset[-1]-test_liabiltiy[-1]]
+
+    PV_asset_t1t2_text = [str('${:,.2f}'.format(PV_asset_t1t2[i]))
+                          for i in range(len(PV_asset_t1t2))]
+    PV_liabilities_t1t2_text = [str('${:,.2f}'.format(
+        PV_liabilities_t1t2[i])) for i in range(len(PV_liabilities_t1t2))]
+    PV_ALMis_text = [str('${:,.2f}'.format(PV_ALMis[i]))
+                     for i in range(len(PV_ALMis))]
+
+    movement_asset = [str(round(test_asset_perc[i]*100, 1)) +
+                      ' bps' for i in range(len(test_asset_perc))]
+    movement_liability = [str(round(test_liabiltiy_perc[i]*100, 1)) +
+                          ' bps' for i in range(len(test_liabiltiy_perc))]
+
+    table_u = BE.fig.data[-2]
+    table_b = BE.fig.data[-1]
+
+    with BE.fig.batch_update():
+        BE.fig.data[0].y = ALM_kit.yfit_beta(BE.tPar, fit_par_t1)
+        BE.fig.data[1].y = ALM_kit.yfit_beta(BE.tPar, fit_par_t2)
+        BE.fig.data[2].y = y1
+        BE.fig.data[3].y = y2
+        BE.fig.data[4].y = cf_bond
+
+        table_u.cells.values = [['Yield 1', 'Yield 2'],
+                                PV_asset_t1t2_text,
+                                PV_liabilities_t1t2_text,
+                                PV_ALMis_text]
+
+        table_b.cells.values = [['Level', 'Slope', 'Curvature', 'Tau', 'TOTAL'],
+                                movement_asset,
+                                movement_liability]
+
+    return BE.fig, duration_asset_text, duration_liability_text,str(np.round(cf_weights/np.sum(cf_weights), 4)).strip('[]'), time_range_t1t2
+
+if __name__ == "__main__":
+	app.run_server(debug=True)
---
+++ utility_exante.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 09:34:50 2020
+
+@author: 330411836
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 08:41:38 2020
+
+@author: 330411836
+"""
+#Added 1) YTM calculator and 2) duration optimizer
+
+#To dos:
+# 1) change the table to reflect movements in returns (asset allocation)
+# 2) Add key duration labels to the chart (key duration movements)
+# 3) add a button to do auto duration match (optimization)
+# 4) quantify the YTM rate of the asset (return rate)
+# 5) Add cashflow tab
+
+#%% Import functions and data
+import ALM_kit
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+from scipy.optimize import minimize
+import quandl
+
+df_YieldCurve = quandl.get("USTREASURY/YIELD", authtoken="4_zrDSANo7hMt_uhyzQy")
+df_YieldCurve.reset_index(level=0, inplace=True)
+df_YieldCurve['Date'] = pd.to_datetime(df_YieldCurve['Date'], format="%m/%d/%Y")
+
+df_mort = pd.read_csv("Mx_2019.csv", sep=',')
+
+#%%
+calendar_t_all = df_YieldCurve['Date']
+t1 = '2019-01-02'
+t2 = '2020-04-02'
+x_range = [t1,t2]
+i_range = np.where((calendar_t_all > t1) & (calendar_t_all < t2))
+
+#%% Fitting historical yield curves using NS formula
+t = np.array([0.08333333, 0.16666667, 0.25, 0.5, 1, 2, 3, 5, 7, 10, 20, 30])
+y = np.array(df_YieldCurve.iloc[:, 1:]).astype(float)[i_range]
+t_cal = calendar_t_all.iloc[i_range]
+fit_par = np.apply_along_axis(lambda x: ALM_kit.ns_par(t, x), axis=1, arr=y)
+
+#%% Cashflow for asset and labilities
+cf_bond_L, t_bond_L = ALM_kit.bond_cashflow(1000, 30, 2.5, 1)
+cf_bond_M, t_bond_M = ALM_kit.bond_cashflow(1000, 10, 2, 1)
+cf_bond_S, t_bond_S = ALM_kit.bond_cashflow(1000, 2, 1, 1)
+cf_bonds = np.array([cf_bond_L,
+                     np.append(cf_bond_M, np.repeat(
+                         0, cf_bond_L.shape[0]-cf_bond_M.shape[0])),
+                     np.append(cf_bond_S, np.repeat(0, cf_bond_L.shape[0]-cf_bond_S.shape[0]))])
+cf_weights = np.array([1.8, 0.2, 2.5])  # Bond weight (Duration matched)
+
+cf_singleasset = np.dot(cf_weights, cf_bonds)
+cf_liability, t_liability = ALM_kit.liability_cashflow(
+    df_mort.loc[60:]['Total'], 3000*12)
+
+#%% PV at time 1
+t1 = 1
+
+# Present value of liability
+PV_liabilities, dur_liabilities = ALM_kit.PV_cashflow(
+    cf_liability, t_liability, fit_ns=fit_par[t1])
+
+# Present value of asset
+PV_asset_single, dur_asset = ALM_kit.PV_cashflow(
+    cf_singleasset, t_bond_L, fit_ns=fit_par[t1])
+N_bond = PV_liabilities/PV_asset_single
+PV_asset, dur_asset = ALM_kit.PV_cashflow(
+    N_bond*cf_singleasset, t_bond_L, fit_ns=fit_par[t1])
+
+#Funding ratio test
+FR = PV_asset/PV_liabilities
+print(FR)
+
+
+# %% Factor analysis between t1 and t2
+t2 = 40
+
+test_asset, test_liabiltiy, dur_asset_t2, dur_liabilities_t2 = ALM_kit.FactorAnalysis(
+    fit_par, t1, t2,
+    cf_asset=(N_bond*cf_singleasset), t_asset=t_bond_L,
+    cf_liability=cf_liability, t_liability=t_liability
+)
+
+test_asset_perc = np.append(
+    np.exp(np.diff(np.log(test_asset)))-1, test_asset[-1]/test_asset[0]-1)*100
+test_liabiltiy_perc = np.append(np.exp(
+    np.diff(np.log(test_liabiltiy)))-1, test_liabiltiy[-1]/test_liabiltiy[0]-1)*100
+
+PV_asset_t1t2 = [test_asset[0], test_asset[-1]]
+PV_liabilities_t1t2 = [test_liabiltiy[0], test_liabiltiy[-1]]
+PV_ALMis = [test_asset[0]-test_liabiltiy[0], test_asset[-1]-test_liabiltiy[-1]]
+
+PV_asset_t1t2_text = [str('${:,.2f}'.format(PV_asset_t1t2[i]))
+                      for i in range(len(PV_asset_t1t2))]
+PV_liabilities_t1t2_text = [str('${:,.2f}'.format(
+    PV_liabilities_t1t2[i])) for i in range(len(PV_liabilities_t1t2))]
+PV_ALMis_text = [str('${:,.2f}'.format(PV_ALMis[i]))
+                 for i in range(len(PV_ALMis))]
+
+movement_asset = [str(round(test_asset_perc[i]*100, 2)) +
+                  ' bps' for i in range(len(test_asset_perc))]
+movement_liability = [str(round(test_liabiltiy_perc[i]*100, 2)) +
+                      ' bps' for i in range(len(test_liabiltiy_perc))]
+
+
+#%% Figures
+
+tPar = np.linspace(0, 30, 50)
+key_dur = np.array([2, 10, 30])
+key_weights = 25*cf_weights/np.sum(cf_weights)
+
+yield_t1 = ALM_kit.yfit_beta(tPar, fit_par[t1])
+yield_t2 = ALM_kit.yfit_beta(tPar, fit_par[t1])
+y_line = [np.min([yield_t1.min(), yield_t2.min()]),
+          np.max([yield_t1.max(), yield_t2.max()])]
+
+
+fig = make_subplots(
+    rows=3, cols=2,
+    subplot_titles=("Yield Curve & Cash Flow", "Performance Attribution"),
+    specs=[
+        [{"type": "xy", "secondary_y": True, "rowspan": 3},{"type": "table"}],
+        [None, {"type": "table", "rowspan": 2}],
+        [None, None]
+    ]
+)
+
+#Yield Curves
+fig.add_trace(go.Scatter(x=tPar, y=ALM_kit.yfit_beta(
+    tPar, fit_par[t1]), mode='lines', line=dict(color='red'), showlegend=False), row=1, col=1)
+fig.add_trace(go.Scatter(x=tPar, y=ALM_kit.yfit_beta(
+    tPar, fit_par[t2]), mode='lines', line=dict(color='blue'), showlegend=False), row=1, col=1)
+
+fig.add_trace(go.Scatter(x=t, y=y[t1], mode='markers', marker=dict(
+    color='red'), name='Raw yield 1'), row=1, col=1)
+fig.add_trace(go.Scatter(x=t, y=y[t2], mode='markers', marker=dict(
+    color='blue'), name='Raw yield 2'), row=1, col=1)
+
+
+#Cash flow
+x_a = t_bond_L
+y_a = N_bond*cf_singleasset
+y_l = cf_liability
+x_l = t_liability
+
+fig.add_trace(go.Scatter(x=x_a, y=y_a, visible=False,line=dict(color='green'),
+                         name='Cashflow - Asset'), secondary_y=False)
+fig.add_trace(go.Scatter(x=x_l, y=y_l, visible=False,line=dict(color='black'),
+                         name='Cashflow - Liability'), secondary_y=True)
+
+fig.add_trace(go.Table(
+    header=dict(values=list(['PV', 'Asset', 'Liability', 'AL-Mismatch']),
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[['Yield 1', 'Yield 2'],
+                       PV_asset_t1t2_text,
+                       PV_liabilities_t1t2_text,
+                       PV_ALMis_text],
+               fill_color='lavender',
+               align='left')),
+              row=1, col=2)
+
+fig.add_trace(go.Table(
+    header=dict(values=list(['%Movement <br>(y1 to y2)', 'Asset', 'Liability']),
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[['Level', 'Slope', 'Curvature', 'Tau', '<b>Total</b>'],
+                       movement_asset,
+                       movement_liability],
+               fill_color='lavender',
+               align='left')),
+              row=2, col=2)
+
+#Button switch
+fig.update_layout(
+    autosize=False,
+    width=1200,
+    height=500,
+    updatemenus=[
+        dict(
+            buttons=list([
+                dict(label="Yield Curve",
+                     method="update",
+                     args=[{'visible': [True, True, True, True, False, False]}
+                           ]
+                     ),
+                dict(label="Cash flow",
+                     method="update",
+                     args=[
+                         {'visible': [False, False, False, False, True, True]}
+                     ]
+                     )
+            ]
+            ), yanchor="top", xanchor="left", y=1.25, x=0
+        )
+    ])
+
+fig.update_layout(legend_orientation="h")
+
+
+
+
+# %%
